38c9912c3d1ec961e8b76643c2e46bcd606a24c3
tongplw <bangkok.tong@gmail.com>
2021-01-17 13:51:21 +0700

    Updates

---
+++ precache-manifest.b7bb1ba8794a6fdfa848625736e352bb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a90df99571cf1bbbfd50772eea2a07f9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "47e39aa4db7a1840ac2b",
+    "url": "/static/js/2.07b56592.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.07b56592.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa91a1face43a218de30",
+    "url": "/static/js/main.bd4ddb68.chunk.js"
+  },
+  {
+    "revision": "4cb7578dc5189bc2161c",
+    "url": "/static/js/runtime-main.15d459b4.js"
+  }
+]);
